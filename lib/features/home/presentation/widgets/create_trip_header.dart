be6ea6942dc88eee645a34aba32e5f5d56c3e4b4
bahaasoubh <bahaa.s@input.ae>
2023-06-10 14:19:49 +0400

    merge with validate date

--- lib/features/home/presentation/widgets/create_trip_header.dart
+++ lib/features/home/presentation/widgets/create_trip_header.dart
@@ -6,9 +6,6 @@ import '../../../../core/constants/colors/app_colors.dart';
 
 class CreateTripHeader extends StatelessWidget {
   final String?image;
-   // bool isThereVerifyEmail;
-   // Function? onTapBack;
-
    CreateTripHeader({super.key,this.image});
   @override
   Widget build(BuildContext context) {
@@ -22,28 +19,6 @@ class CreateTripHeader extends StatelessWidget {
                 image: AssetImage(image!),fit: BoxFit.cover),
           ),
         ),
-        // Padding(
-        //   padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-        //   child: Container(
-        //       padding: const EdgeInsets.all(8),
-        //       decoration: const BoxDecoration(
-        //         shape: BoxShape.circle,
-        //         color: AppColors.formsBackground,
-        //       ),
-        //       child: InkWell(
-        //         onTap: () {
-        //           if (isThereVerifyEmail) {
-        //             onTapBack!();
-        //           } else {
-        //             Navigator.pop(context);
-        //           }
-        //         },
-        //         child: const Icon(
-        //           Icons.arrow_back_ios_new,
-        //           color: AppColors.white,
-        //         ),
-        //       )),
-        // ),
       ],
     );
   }
